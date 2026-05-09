92f85ce42dc24e68e8f6744c365fa5d2714b4059
Pepote145 <jjesussuarezzz@gmail.com>
2026-05-09 13:54:55 +0100

    Estructura Maven multimodulo

---
+++ app/src/main/java/org/ulpgc/dacd/app/Main.java
@@ -0,0 +1,7 @@
+package org.ulpgc.dacd.app;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("PioPioFly iniciado correctamente");
+    }
+}
---
+++ domain/src/main/java/org/ulpgc/dacd/domain/package-info.java
@@ -0,0 +1,1 @@
+package org.ulpgc.dacd.domain;
